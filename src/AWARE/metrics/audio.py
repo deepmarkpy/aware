d8ed07459b0787aa8a2a5184012be40af51f9630
LazarStanarevic01 <lazar.stanarevic@deepmark.me>
2025-09-02 13:40:14 +0200

    feat: added BER, PESQ i STOI to metrics

--- src/AWARE/metrics/audio.py
+++ src/AWARE/metrics/audio.py
@@ -1,19 +1,70 @@
-from deltamark.interfaces.metrics import BaseMetrics
+from AWARE.interfaces.metrics import BaseMetrics
 import torch
 import numpy as np
 from pesq import pesq
+from pystoi import stoi
 import librosa
 
+class BER(BaseMetrics):
+    def __call__(self, output: np.ndarray | torch.Tensor, target: np.ndarray | torch.Tensor) -> float:
+        if isinstance(output, torch.Tensor):
+            output = output.numpy()
+        if isinstance(target, torch.Tensor):
+            target = target.numpy()
+
+        ber = np.mean( output != target ) * 100
+
+        return ber
+    
 class PESQ(BaseMetrics):
     def __call__(self, output: np.ndarray | torch.Tensor, target: np.ndarray | torch.Tensor, sampling_rate: int) -> float:
         if isinstance(output, torch.Tensor):
             output = output.numpy()
         if isinstance(target, torch.Tensor):
             target = target.numpy()
-        resampled_output = librosa.resample(output, orig_sr=sampling_rate, target_sr=16000)
-        resampled_target = librosa.resample(target, orig_sr=sampling_rate, target_sr=16000)
-        return pesq(16000, resampled_output, resampled_target, 'wb')
-    
+
+        if len(output.shape) == 2 and output.shape[1]==2:
+            output_ = np.mean(output, axis=1)
+            target_ = np.mean(target, axis=1)
+        else:
+            output_ = output
+            target_ = target
+
+        len_ = min(len(output_), len(target_))
+        output_ = output_[:len_]
+        target_ = target_[:len_]
+        
+        resampled_output = librosa.resample(output_, orig_sr=sampling_rate, target_sr=16000)
+        resampled_target = librosa.resample(target_, orig_sr=sampling_rate, target_sr=16000)
+        return pesq(16000, resampled_target, resampled_output, 'wb')
+
+
+class STOI(BaseMetrics):
+    def __call__(self, output: np.ndarray | torch.Tensor, target: np.ndarray | torch.Tensor, sampling_rate: int) -> float:
+        if isinstance(output, torch.Tensor):
+            output = output.numpy()
+        if isinstance(target, torch.Tensor):
+            target = target.numpy()
+
+        if len(output.shape) == 2 and output.shape[1]==2:
+            output_ = np.mean(output, axis=1)
+            target_ = np.mean(target, axis=1)
+        else:
+            output_ = output
+            target_ = target
+
+        len_ = min(len(output_), len(target_))
+        output_ = output_[:len_]
+        target_ = target_[:len_]
+        
+        resampled_output = librosa.resample(output_, orig_sr=sampling_rate, target_sr=16000)
+        resampled_target = librosa.resample(target_, orig_sr=sampling_rate, target_sr=16000)
+        score = stoi(resampled_target, resampled_output, 16000)
+
+        return float(score)
+
+
+
 class SNR(BaseMetrics):
     def __call__(self, output: np.ndarray | torch.Tensor, target: np.ndarray | torch.Tensor) -> float:
 
@@ -22,6 +73,17 @@ class SNR(BaseMetrics):
         if isinstance(target, torch.Tensor):
             target = target.numpy()
         
-        if np.all(output == target):
+        if len(output.shape) == 2 and output.shape[1]==2:
+            output_ = np.mean(output, axis=1)
+            target_ = np.mean(target, axis=1)
+        else:
+            output_ = output
+            target_ = target
+
+        len_ = min(len(output_), len(target_))
+        output_ = output_[:len_]
+        target_ = target_[:len_]
+
+        if np.all(output_ == target_):
             return float('inf')
-        return 10 * np.log10(np.mean(output**2) / np.mean((output - target)**2))
+        return 10 * np.log10(np.mean(output_**2) / np.mean((output_ - target_)**2))
