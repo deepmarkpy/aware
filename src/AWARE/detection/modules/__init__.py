edcf2c7c5dde882eb378065ae2e9db732cabfb81
LazarStanarevic01 <lazar.stanarevic@deepmark.me>
2025-09-02 16:02:19 +0200

    feat: solving some small bug, nothing serious

--- src/AWARE/detection/modules/__init__.py
+++ src/AWARE/detection/modules/__init__.py
@@ -1,4 +1,5 @@
 from .conv1d import Conv1dBlock
 from .mel import MelFilterBankLayer, MelFilterBankLayerEinsum
+from .globalStandardize import GlobalStandardize
 
-__all__ = ['Conv1dBlock', 'MelFilterBankLayer', 'MelFilterBankLayerEinsum']
+__all__ = ['Conv1dBlock', 'MelFilterBankLayer', 'MelFilterBankLayerEinsum', 'GlobalStandardize']
